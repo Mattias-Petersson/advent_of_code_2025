a9450e782a6abf43664a1f000e2379d56c848295
Mattias Petersson <Mattias.Petersson93@gmail.com>
2026-01-06 13:51:48 +0100

    Day 3 part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -5,7 +5,7 @@ use advent_of_code_2025::read_input;
 pub fn exercise() {
     match get_batteries() {
         Ok(batteries) => {
-            let sum: u32 = batteries.iter().map(|b| get_largest_joltage(b)).sum();
+            let sum: u64 = batteries.iter().map(|b| get_largest_joltage(b)).sum();
             println!("{}", sum);
         }
         Err(e) => eprintln!("Error {e}"),
@@ -18,17 +18,29 @@ fn get_batteries() -> Result<Vec<String>, Box<dyn Error>> {
     Ok(res)
 }
 
-fn get_largest_joltage(batteries: &str) -> u32 {
-    let mut best_pair = 0;
-    let mut best_first = 0;
+fn get_largest_joltage(batteries: &str) -> u64 {
+    let stack_size = 12;
+    let digits: Vec<char> = batteries.chars().collect();
+    let n = digits.len();
+    let mut stack: Vec<char> = Vec::new();
 
-    for c in batteries.chars() {
-        let d = c.to_digit(10).unwrap();
-        best_pair = best_pair.max(best_first * 10 + d);
-        best_first = best_first.max(d);
+    for (i, &c) in digits.iter().enumerate() {
+        while let Some(&top) = stack.last() {
+            let remaining = n - i;
+            let needed = stack_size - stack.len();
+
+            if top < c && remaining > needed {
+                stack.pop();
+            } else {
+                break;
+            }
+        }
+
+        if stack.len() < stack_size {
+            stack.push(c);
+        }
     }
-
-    best_pair
+    stack.into_iter().collect::<String>().parse().unwrap()
 }
 #[cfg(test)]
 mod tests {
@@ -37,25 +49,18 @@ mod tests {
     #[test]
     fn test_adjacent_bank() {
         let joltage = get_largest_joltage("987654321111111");
-        assert_eq!(joltage, 98);
+        assert_eq!(joltage, 987654321111);
     }
 
     #[test]
     fn test_ends_bank() {
         let joltage = get_largest_joltage("811111111111119");
-        assert_eq!(joltage, 89);
+        assert_eq!(joltage, 811111111119);
     }
 
     #[test]
     fn test_arbitrary_bank() {
         let joltage = get_largest_joltage("818181911112111");
-        assert_eq!(joltage, 92);
-    }
-
-    #[test]
-    fn test_sum_banks() {
-        let first_joltage = get_largest_joltage("987654321111111");
-        let second_joltage = get_largest_joltage("811111111111119");
-        assert_eq!(first_joltage + second_joltage, 98 + 89);
+        assert_eq!(joltage, 888911112111);
     }
 }
