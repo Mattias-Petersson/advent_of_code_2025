0997ee85f3030cc692c7bd9c8c8811cb90671aea
Mattias Petersson <Mattias.Petersson93@gmail.com>
2026-01-07 18:22:59 +0100

    Day 4 basic test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
-    let days_impl = 3;
+    let days_impl = 4;
     let args = std::env::args().nth(1);
     if let Some(day) = args {
         let day: u32 = day.parse().unwrap();
@@ -26,6 +27,7 @@ fn run_day(day: u32) {
         1 => day1::exercise(),
         2 => day2::exercise(),
         3 => day3::exercise(),
+        4 => day4::exercise(),
         _ => unimplemented!(),
     }
 }
