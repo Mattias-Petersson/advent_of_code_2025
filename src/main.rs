e3b77bbc0dabfbc5206a0a50b2d921566f8e682e
Mattias Petersson <Mattias.Petersson93@gmail.com>
2026-01-07 17:32:49 +0100

    Change main fn to enable running specific days

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,29 @@ mod day2;
 mod day3;
 
 fn main() {
-    day1::exercise();
-    day2::exercise();
-    day3::exercise();
+    let days_impl = 3;
+    let args = std::env::args().nth(1);
+    if let Some(day) = args {
+        let day: u32 = day.parse().unwrap();
+        if day > 0 && day < days_impl {
+            println!("Running day {day}");
+            run_day(day);
+        }
+    } else {
+        println!("Running all days");
+        for day in 1..=days_impl {
+            print!("Day {day}:");
+            run_day(day);
+            println!();
+        }
+    }
+}
+
+fn run_day(day: u32) {
+    match day {
+        1 => day1::exercise(),
+        2 => day2::exercise(),
+        3 => day3::exercise(),
+        _ => unimplemented!(),
+    }
 }
