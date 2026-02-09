5b2f225b3c81aa6cedd496de53631b9a94dcd339
Mattias Petersson <Mattias.Petersson93@gmail.com>
2026-02-09 17:47:05 +0100

    Day 9 part 1

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -1,13 +1,17 @@
+use std::{error::Error, io::BufRead};
+
+use advent_of_code_2025::read_input;
+
 struct RedTile {
-    x: u32,
-    y: u32,
+    x: u64,
+    y: u64,
 }
 
 impl RedTile {
-    fn new(x: u32, y: u32) -> Self {
+    fn new(x: u64, y: u64) -> Self {
         RedTile { x, y }
     }
-    fn area(&self, p: &RedTile) -> u32 {
+    fn area_bounds(&self, p: &RedTile) -> u64 {
         let dx = self.x.abs_diff(p.x) + 1;
         let dy = self.y.abs_diff(p.y) + 1;
         dx * dy
@@ -15,11 +19,35 @@ impl RedTile {
 }
 
 pub fn exercise() {
-    unimplemented!();
+    match read_red_tiles() {
+        Ok(res) => {
+            println!("Largest area is: {}", find_largest_area(&res));
+        }
+        Err(e) => eprintln!("Error: {e}"),
+    }
 }
 
-fn find_largest_area(points: &[RedTile]) -> u32 {
-    unimplemented!();
+fn read_red_tiles() -> Result<Vec<RedTile>, Box<dyn Error>> {
+    let input = read_input("day9")?;
+    input
+        .lines()
+        .map(|line| {
+            let values: Vec<u64> = line?
+                .split(',')
+                .map(|s| s.parse::<u64>())
+                .collect::<Result<_, _>>()?;
+            Ok(RedTile::new(values[0], values[1]))
+        })
+        .collect()
+}
+
+fn find_largest_area(points: &[RedTile]) -> u64 {
+    points
+        .iter()
+        .enumerate()
+        .flat_map(|(i, p1)| points.iter().skip(i + 1).map(move |p2| p1.area_bounds(p2)))
+        .max()
+        .unwrap_or(0)
 }
 
 #[cfg(test)]
@@ -43,7 +71,7 @@ mod tests {
         let points = setup();
         let p1 = &points[1];
         let p2 = &points[5];
-        let res = p1.area(p2);
+        let res = p1.area_bounds(p2);
         assert_eq!(res, 50);
     }
     #[test]
